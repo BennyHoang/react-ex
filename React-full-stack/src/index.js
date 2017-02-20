35af880bd773197ceab9afbe7bfb2054252b8e15
Benny Hoang <hoaben13@student.westerdals.no>
2017-02-20 12:51:14 +0100

    added sass middleware

--- React-full-stack/src/index.js
+++ React-full-stack/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import data from './testData';
 import App from './components/App';
 
+
 ReactDOM.render(
-    <App />,
+    <App contests={data.contests} />,
     document.getElementById('root')
 );
-
-setTimeout(() =>{
-    ReactDOM.render(
-        <h2>....</h2>,
-        document.getElementById('root')
-    );
-}, 4000);
