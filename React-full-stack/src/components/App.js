8d7592108303377902b0c2f22977c42e101d7dc1
Benny Hoang <hoaben13@student.westerdals.no>
2017-02-20 13:30:41 +0100

    added axios for ajax calls

--- React-full-stack/src/components/App.js
+++ React-full-stack/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 class App extends React.Component {
@@ -6,21 +7,27 @@ class App extends React.Component {
         pageHeader: "Naming Contest",
         contests: []
     };
-    componentDidMount(){
-        this.setState({
-            contests: data.contests
-        });
+    componentDidMount() {
+        axios.get('api/contests')
+            .then(res => {
+                this.setState({
+                    contests: res.data.contests
+                });
+                
+            })
+            .catch(console.error);
+
     }
-    componentWillUnmount(){
+    componentWillUnmount() {
 
     }
     render() {
         return (
             <div className="App">
-                <Header message={ this.state.pageHeader} />
+                <Header message={this.state.pageHeader} />
                 <div>
                     {this.state.contests.map(contest =>
-                        <ContestPreview key={contest.id}  {...contest} />                    
+                        <ContestPreview key={contest.id}  {...contest} />
                     )}
                 </div>
             </div>
